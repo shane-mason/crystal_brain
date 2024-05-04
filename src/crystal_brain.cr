58de0e10cbb09ec484bbcc132c5397c82a5ea5c5
Shane C Mason <shane.c.mason@gmail.com>
2024-05-04 13:59:19 -0700

    Refactored CLI into own file

--- src/crystal_brain.cr
+++ src/crystal_brain.cr
@@ -2,7 +2,7 @@ require "colorize"
 require "option_parser"
 
 module CrystalBrain
-  VERSION = "0.1.0"
+
 
   #states
   DEAD  = 0
@@ -99,47 +99,6 @@ module CrystalBrain
 
 
   end # class brain
-
-  iterations = 200
-  sleep_time = 0.25
-  x_size = 10
-  y_size = 10
-  OptionParser.parse do |parser|
-
-    parser.banner = "Welcome to Crystal Brain"
-    parser.on "-v", "--version", "Show version" do
-      puts "version 1.0"
-      exit
-    end
-    parser.on "-h", "--help", "Show help" do
-      puts parser
-      exit
-    end
-    parser.on "-i ITERS", "--iterations ITERS", "Iteration count" do |iters|
-      iterations = iters.to_i
-    end
-    parser.on "-r SIZE", "--rowsize SIZE", "Board size in rows" do |xsz|
-      x_size = xsz.to_i
-    end
-    parser.on "-c SIZE", "--colsize SIZE", "Board size in columns" do |ysz|
-      y_size = ysz.to_i
-    end
-    parser.on "-t TIME", "--time TIME", "Animation sleep time" do |tme|
-      sleep_time = tme.to_f
-    end
-
-  end
-
-  brain = Brain.new "MyTest", x_size.to_i, y_size.to_i
-  count = 0
-  while iterations > 0
-    brain.simple_print
-    puts "Iterating: " + count.to_s
-    iterations -= 1
-    count += 1
-    sleep sleep_time
-    brain.tick
-  end
 
 
 end
